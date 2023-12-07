1ab521674f6c275b7ad954b4fb529a55633d91f9
bastianfbr <badmaxyt@gmail.com>
2023-12-07 22:10:52 +0100

    feat: #1 add header with logo

---
+++ app/components/header.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import logo from "../../public/meuch_logo.svg";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="navbar bg-white">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Meuch</a>
+        <Image src={logo} alt="Meuch" width={100} height={100} />
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1 text-black text-3xl">
+          <li>
+            <Link href="/">Accueil</Link>
+          </li>
+          <li>
+            <Link href="/quizz">Quizz</Link>
+          </li>
+          <li>
+            <Link href="/about">À propos</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
