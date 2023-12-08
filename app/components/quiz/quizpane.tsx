cddbb6a8629823f3a8cfa5fbab46fb862e69b8c2
Samy Mosa <samymosa02@gmail.com>
2023-12-08 05:35:31 +0100

    fix: #6 Better focus handling

--- app/components/quiz/quizpane.tsx
+++ app/components/quiz/quizpane.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import questions from "../../../public/quizData.json";
 
 export default function QuizPane() {
   const [isQuestionAnswered, setIsQuestionAnswered] = useState(false);
   const [questionNumber, setQuestionNumber] = useState(0);
   const [choice, setChoice] = useState(-1);
+  const firstAnswerRef = useRef<HTMLButtonElement>(null);
+  const nextQuestionRef = useRef<HTMLButtonElement>(null);
 
   const nextQuestion = () => {
     setIsQuestionAnswered(false);
     setChoice(-1);
     setQuestionNumber((questionNumber + 1) % questions.length);
+    firstAnswerRef.current?.focus();
   }
 
   const getAnswerClass = (index: number) => {
@@ -37,6 +40,9 @@ export default function QuizPane() {
 
     setIsQuestionAnswered(true);
     setChoice(choice);
+    setTimeout(() => {
+      nextQuestionRef.current?.focus();
+    }, 100);
   };
 
   return (
@@ -45,12 +51,13 @@ export default function QuizPane() {
       <section className={`bg-white border border-gray-300 shadow-md rounded-xl w-2/3 collapse ${isQuestionAnswered && "collapse-open"}`}>
         <div className="collapse-title text-xl font-medium p-8">
           <h2 className="text-2xl font-bold text-center mb-6">{questions[questionNumber]?.question}</h2>
-          <div className="grid grid-cols-2 gap-4 gap-x-4">
+          <div className="answers grid grid-cols-2 gap-4 gap-x-4">
             {questions[questionNumber]?.answers.map((option: string, index: number) => (
               <button
                 key={index}
                 className={`btn btn-primary ${getAnswerClass(index)}`}
-                onClick={handleChoice.bind(null, index)}>
+                onClick={handleChoice.bind(null, index)}
+                ref={index === 0 ? firstAnswerRef : null}>
                 {option}
               </button>
             ))}
@@ -62,7 +69,7 @@ export default function QuizPane() {
             <p className="text-sm text-gray-700">
               {questions[questionNumber].information}
             </p>
-            <button className="btn btn-sm btn-primary" onClick={nextQuestion}>Question suivante</button>
+            <button className="btn btn-sm btn-primary" onClick={nextQuestion} ref={nextQuestionRef}>Question suivante</button>
           </div>
         </div>
       </section>
