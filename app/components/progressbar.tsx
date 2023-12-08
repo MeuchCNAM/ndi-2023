0a7e77f5fca6451969b83ddb7f64fbab0f847bf3
Laduve-prog <duvernay.benoit@gmail.com>
2023-12-08 02:26:38 +0100

    fix: #9 progress bar

--- app/components/progressbar.tsx
+++ app/components/progressbar.tsx
@@ -18,16 +18,10 @@ const Co2ProgressBar: React.FC<Co2ProgressBarProps> = ({ co2Value }) => {
     <div className="text-center">
       <h1 className="font-bold text-xl mb-2">Taux de CO2</h1>
       <LinearProgress
+        className="h-10 rounded-lg"
         variant="determinate"
         value={progress}
-        sx={{
-          height: '20px', // Increase the height
-          borderRadius: '10px', // Add border-radius
-          border: '1px solid #000000', // Add a 1px solid border with black color
-          '& .MuiLinearProgress-barColorPrimary': {
-            backgroundColor: '#ff0000', // Red color for the bar
-          },
-        }}
+        color="secondary" // Use the 'secondary' color as the filling color
       />
     </div>
   );
