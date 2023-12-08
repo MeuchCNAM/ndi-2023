cdfdf06a5609782c7b7925a94ce1a3f1a3ec7bd0
bastianfbr <badmaxyt@gmail.com>
2023-12-08 06:45:46 +0100

    feat: #16 add i18n quiz logic

--- app/[locale]/quiz/page.tsx
+++ app/[locale]/quiz/page.tsx
@@ -1,10 +1,49 @@
 import QuizPane from "@/app/components/quiz/quizpane";
+import { useTranslations } from "next-intl";
 import React from "react";
+import tradDE from "../../../messages/de.json";
+import tradEN from "../../../messages/en.json";
+import tradFR from "../../../messages/fr.json";
+
+const questionsDE = tradDE.Game;
+const questionsEN = tradEN.Game;
+const questionsFR = tradFR.Game;
 
 export default function Quiz() {
-  return (
-    <main>
-      <QuizPane />
-    </main>
-  );
+  const trad = useTranslations("Index");
+  const language = trad("language");
+  const quizTrad = useTranslations("Quiz");
+
+  switch (language) {
+    case "de":
+      return (
+        <main>
+          <QuizPane
+            questions={questionsDE}
+            title={quizTrad("title")}
+            next={quizTrad("next")}
+          />
+        </main>
+      );
+    case "en":
+      return (
+        <main>
+          <QuizPane
+            questions={questionsEN}
+            title={quizTrad("title")}
+            next={quizTrad("next")}
+          />
+        </main>
+      );
+    case "fr":
+      return (
+        <main>
+          <QuizPane
+            questions={questionsFR}
+            title={quizTrad("title")}
+            next={quizTrad("next")}
+          />
+        </main>
+      );
+  }
 }
